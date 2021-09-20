30d84c38642b9749b51b53b7c5706641519f49c5
Douglas Duarte <douglasduarte.eng@gmail.com>
2021-09-20 15:15:43 -0300

    :up: Navigation bar with React router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
     return (
         <div className="App">
-            <NavBar />
+            <Router>
+                <NavBar />
+                <Switch>
+                    <Route path="/" exact component={Home}></Route>
+                </Switch>
+            </Router>
         </div>
     );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,26 @@
-import "./NavBar.css";
-import logo from "./logo.png";
+import React from "react";
+import "../styles/NavBar.css";
+import Logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 function NavBar() {
     return (
         <div className="nav">
-            <img src={logo} alt="logo" className="nav_logo" />
-            <ul className="nav_links">
-                <li className="nav_item">
-                    <a className="nav_link" href="#">
-                        Home
-                    </a>
-                </li>
-                <li className="nav_item">
-                    <a className="nav_link" href="#">
-                        About
-                    </a>
-                </li>
-                <li className="nav_item">
-                    <a className="nav_link" href="#">
-                        Testimonials
-                    </a>
-                </li>
-                <li className="nav_item">
-                    <a className="nav_link" href="#">
-                        Contact us
-                    </a>
-                </li>
-            </ul>
+            <img src={Logo}></img>
+            <div className="nav_links">
+                <Link className="nav_link" to="/">
+                    Início
+                </Link>
+                <Link className="nav_link" to="/">
+                    Sobre
+                </Link>
+                <Link className="nav_link" to="/">
+                    Depoimentos
+                </Link>
+                <Link className="nav_link" to="/">
+                    Contato
+                </Link>
+            </div>
         </div>
     );
 }
