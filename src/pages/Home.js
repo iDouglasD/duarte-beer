6a94f5823be44b4ed9fa8b0d068eeae0c88e9d88
Douglas Duarte <douglasduarte.eng@gmail.com>
2021-09-21 00:02:05 -0300

    :up: Update home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "../styles/Home.css";
+import Beer from "../assets/beer.png";
 import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div className="home">
-            <div className="container">
-                <div className="content">
-                    <h1>Duarte's Beer</h1>
-                    <p>TEMOS álcool EM COMUM!</p>
-                    <Link to="/contato">
-                        <button>PEÇA AGORA</button>
-                    </Link>
-                </div>
-                <div className="icons_bg"></div>
+        <div className="container">
+            <div className="content_container">
+                <h1 ClassName="highlight">Duarte's Beer</h1>
+                <p>TEMOS álcool EM COMUM!</p>
+                <Link to="/contato">
+                    <button>PEÇA AGORA</button>
+                </Link>
+            </div>
+            <div className="img_wrapper">
+                <img src={Beer} alt="Copo de cerveja"></img>
             </div>
         </div>
     );
